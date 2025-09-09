dfb1ad316ee1c88b33ae8e79a4e8083327854869
Punitha Kathirvel <149648026+punitha-96@users.noreply.github.com>
2025-09-09 14:57:59 +0530

    Update rps_game.py

--- rock_paper_scissors/rps_game.py
+++ rock_paper_scissors/rps_game.py
@@ -1,1 +1,29 @@
-# Rock Paper Scissors Game
+import random
+
+choices = ['rock','paper','scissors']
+user_score = 0
+computer_score = 0
+
+while True:
+  user = input('Choose Rock, Paper, Scissors or type quit to stop :  ').lower()
+
+  if user == 'quit':
+    break
+  if user not in choices:
+    print('Invalid choice. Please choose Rock, Paper, or Scissors.')
+    continue
+
+  computer = random.choice(choices)
+  print(f'You chose {user}, computer chose {computer}') # Added for clarity
+  if user == computer:
+    print('Its a tie')
+  elif (user == 'rock' and computer == 'scissors') or \
+       (user == 'paper' and computer == 'rock') or \
+       (user == 'scissors' and computer == 'paper'):
+    print('You win!')
+    user_score = user_score + 1
+  else:
+    print('You lose!')
+    computer_score = computer_score + 1
+
+print(f'User score is : {user_score} and computer_score is : {computer_score}')
